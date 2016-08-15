43b9525d62b369947b36fc2d8323bd6782e5bdb7
Ramon Gebben <github@ra-ge.net>
2016-08-15 13:54:20 +0200

    Delete formatTime.spec.js

--- test/unit/formatTime.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import formatTime from '../../src/utils/formatTime';
-
-describe('formatTime', () => {
-  it('should return 12:23:02 for 1463394182741', () => {
-    expect(formatTime(1463394182741)).to.equal('12:23:02');
-  });
-});
